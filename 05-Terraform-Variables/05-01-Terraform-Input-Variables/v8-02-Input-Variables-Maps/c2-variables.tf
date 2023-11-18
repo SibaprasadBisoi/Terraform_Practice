1afae54d8609fc0cf71ed9f78c18c5a4ee083b0a
prasad <sibaprasad.7777@gmail.com>
2023-11-18 23:31:45 +0530

    Added files for mapping the variables

---
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v8-02-Input-Variables-Maps/c2-variables.tf
@@ -0,0 +1,38 @@
+# Input Variables
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ec2_ami_id" {
+  description = "AMI ID"
+  type        = string
+  default     = "ami-*********" # Amazon2 Linux AMI ID
+}
+
+variable "ec2_instance_count" {
+  description = "EC2 Instance Count"
+  type        = number
+  default     = 1
+}
+
+
+variable "ec2_instance_type" {
+  description = "type of ec2 instance"
+  type = list(string)
+  default = {
+    small-apps  = "t2.micro"
+    medium-apps = "t2.medium"
+    large-apps  = "t2.large"
+  }
+}
+
+variable "ec2_instance_tag" {
+  description = "Tag of ec2 instance"
+  type = map(string)
+  default = {
+    "Name"  = "ec2-instance"
+    "Tier"  = "Web"
+  }
+}
