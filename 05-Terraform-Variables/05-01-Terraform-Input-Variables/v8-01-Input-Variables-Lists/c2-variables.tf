dd244e1042ae03e31cee944b98911b8466351e2f
prasad <sibaprasad.7777@gmail.com>
2023-11-18 23:22:38 +0530

    Added files for variable list

---
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v8-01-Input-Variables-Lists/c2-variables.tf
@@ -0,0 +1,25 @@
+# Input Variables
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ec2_ami_id" {
+  description = "AMI ID"
+  type        = string
+  default     = "ami-*********" # Amazon2 Linux AMI ID
+}
+
+variable "ec2_instance_count" {
+  description = "EC2 Instance Count"
+  type        = number
+  default     = 1
+}
+
+# Enter the type when prompted
+variable "ec2_instance_type" {
+  description = "type of ec2 instance"
+  type = list(string)
+  default = [ "t2.small", "t2.micro", "t2.large" ]
+}
